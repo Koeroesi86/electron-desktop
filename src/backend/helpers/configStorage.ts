b105cd6ece3ca18709bfb8391dc29a6ca67f9c2c
Krisztian Korosi <koeroesi86@gmail.com>
2021-05-09 09:03:24 +0100

    Created FileStorage class (for ease of reuse)

--- src/backend/helpers/configStorage.ts
+++ src/backend/helpers/configStorage.ts
@@ -1,56 +1,21 @@
-import fs from "fs";
-import path from "path";
 import { app } from "electron";
-import throttle from "lodash.throttle";
+import FileStorage from "./fileStorage";
 
-const root = app.getPath("userData");
-
-const resolvePath = (fileName: string) => path.resolve(root, fileName);
-
-const throttledStore: { [p: string]: string } = {};
-
-const write = (fileName: string, data: string): void => {
-  fs.writeFileSync(resolvePath(fileName), data, "utf8");
-};
-
-const read = (fileName: string): string => fs.readFileSync(resolvePath(fileName), "utf8");
-
-const exists = (fileName: string): boolean => fs.existsSync(resolvePath(fileName));
-
-const throttledWrite = throttle<(f: string, d: string) => void>(write, 5000);
-
-process.on("beforeExit", () => {
-  throttledWrite.cancel();
-  Object.keys(throttledStore).forEach((p) => write(p, throttledStore[p]));
-});
+const fileStorage = new FileStorage(app.getPath("userData"), 5000);
 
 const configStorage = {
   set: async (fileName: string, data: any): Promise<void> => {
     const target = JSON.stringify(data, null, 2);
 
-    if (throttledStore[fileName] !== target) {
-      throttledStore[fileName] = target;
-      throttledWrite(fileName, target);
-    }
+    fileStorage.throttledWrite(fileName, target);
   },
-  exists: async (fileName: string): Promise<boolean> => {
-    if (throttledStore[fileName]) {
-      return true;
-    }
-
-    return exists(fileName);
-  },
+  exists: async (fileName: string): Promise<boolean> => fileStorage.exists(fileName),
   get: async <T = any>(fileName: string, defaultValue: T): Promise<T> => {
     if (!(await configStorage.exists(fileName))) {
       await configStorage.set(fileName, defaultValue);
     }
 
-    let content = throttledStore[fileName];
-
-    if (!content) {
-      content = read(fileName);
-      throttledStore[fileName] = content;
-    }
+    const content = fileStorage.read(fileName);
 
     return JSON.parse(content) as T;
   },
---
+++ src/backend/helpers/fileStorage.ts
@@ -0,0 +1,67 @@
+import fs from "fs";
+import path from "path";
+import throttle from "lodash.throttle";
+
+type FileWrite = (fileName: string, data: string) => void;
+
+// from node_modules/@types/lodash/common/function.d.ts:371
+interface DebouncedFunc<T extends (...args: any[]) => any> {
+  (...args: Parameters<T>): ReturnType<T> | undefined;
+  cancel(): void;
+  flush(): ReturnType<T> | undefined;
+}
+
+class FileStorage {
+  throttledStore: { [p: string]: string } = {};
+
+  private readonly root: string;
+
+  throttledWrite: FileWrite;
+
+  private readonly writeThrottle: DebouncedFunc<FileWrite>;
+
+  constructor(root: string, writeThrottleInterval: number = 5000) {
+    this.root = root;
+    this.writeThrottle = throttle<FileWrite>(this.write, writeThrottleInterval);
+
+    this.throttledWrite = (fileName: string, data: string) => {
+      if (this.throttledStore[fileName] !== data) {
+        console.log("detected change", fileName, data);
+        this.throttledStore[fileName] = data;
+        this.writeThrottle(fileName, data);
+      }
+    };
+
+    process.on("exit", () => {
+      this.writeThrottle.cancel();
+      Object.keys(this.throttledStore).forEach((p) => this.write(p, this.throttledStore[p]));
+    });
+  }
+
+  private resolvePath = (fileName: string) => path.resolve(this.root, fileName);
+
+  read = (fileName: string): string => {
+    let content = this.throttledStore[fileName];
+
+    if (!content) {
+      content = fs.readFileSync(this.resolvePath(fileName), "utf8");
+      this.throttledStore[fileName] = content;
+    }
+
+    return content;
+  };
+
+  exists = (fileName: string): boolean => {
+    if (this.throttledStore[fileName]) {
+      return true;
+    }
+
+    return fs.existsSync(this.resolvePath(fileName));
+  };
+
+  private write = (fileName: string, data: string): void => {
+    fs.writeFileSync(this.resolvePath(fileName), data, "utf8");
+  };
+}
+
+export default FileStorage;
